3e061a939e40f695d1b2a3237f6bdb3b98968aed
Saran Krishnaraja <krishnaraja.saran@gmail.com>
2021-10-18 01:38:42 -0400

    First Commit

---
+++ Assets/Scripts/GeneticMutationManager.cs
@@ -0,0 +1,283 @@
+﻿// Saran Krishnaraja 100621699
+// Justin Collier 100345263
+// Jarvis Ortega 100625970
+// Bosco Kan 100621465
+
+// Code referenced from:
+// https://www.youtube.com/watch?v=C6SZUU8XQQ0
+// https://www.youtube.com/watch?v=VYQZ-kjP1ec
+// https://www.youtube.com/watch?v=J3_iAC1q1NM&list=WL&index=94&t=0s
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using MathNet.Numerics.LinearAlgebra;
+
+public class GeneticMutationManager : MonoBehaviour
+{
+    // Public Variables
+    public BoatControls boatController;
+
+    public int initPopulation = 85;
+    [Range(0.0f, 1.0f)]
+    public float geneMutationRate = 0.055f;
+
+    public int bestSelection = 8;
+    public int worstSelection = 3;
+    public int numCrossover;
+
+
+    public int currentGen;
+    public int currentGenome = 0;
+
+    // Private Variables
+    private List<int> genePool = new List<int>();
+
+    private int naturallySelected;
+
+    private NeuralNetwork[] population;
+
+    private void Start()
+    {
+        InitPopulation();
+    }
+
+    // Create the initial population of the Neural Network
+    private void InitPopulation()
+    {
+        population = new NeuralNetwork[initPopulation];
+        FillPopulation(population, 0);
+        ResetCurrentGenome();
+    }
+
+    // Reset the current genome
+    private void ResetCurrentGenome()
+    {
+        boatController.ResetNetwork(population[currentGenome]);
+    }
+
+    // Fill the neural network population
+    private void FillPopulation(NeuralNetwork[] newPop, int index)
+    {
+        while (index < initPopulation)
+        {
+            newPop[index] = new NeuralNetwork();
+            newPop[index].Init(boatController.layers, boatController.neurons);
+            index++;
+        }
+    }
+
+    // Call function when AI dies
+    public void Death(float fitness, NeuralNetwork network)
+    {
+
+        if (currentGenome < population.Length - 1)
+        {
+
+            population[currentGenome].fitness = fitness;
+            currentGenome++;
+            ResetCurrentGenome();
+
+        }
+        else
+        {
+            Repopulate();
+        }
+
+    }
+
+    // Repopulate neural network
+    private void Repopulate()
+    {
+        genePool.Clear();
+        currentGen++;
+        naturallySelected = 0;
+        SortPopulation();
+
+        NeuralNetwork[] newPop = BestPopulation();
+
+        Crossover(newPop);
+        Mutation(newPop);
+
+        FillPopulation(newPop, naturallySelected);
+
+        population = newPop;
+
+        currentGenome = 0;
+
+        ResetCurrentGenome();
+
+    }
+    
+    // Mutate the neural network AI 
+    private void Mutation(NeuralNetwork[] newPop)
+    {
+
+        for (int i = 0; i < naturallySelected; i++)
+        {
+
+            for (int c = 0; c < newPop[i].weights.Count; c++)
+            {
+
+                if (Random.Range(0.0f, 1.0f) < geneMutationRate)
+                {
+                    newPop[i].weights[c] = MutationMatrix(newPop[i].weights[c]);
+                }
+
+            }
+
+        }
+
+    }
+
+    // Mutate the matrix
+    Matrix<float> MutationMatrix(Matrix<float> A)
+    {
+
+        int randomPoints = Random.Range(1, (A.RowCount * A.ColumnCount) / 7);
+
+        Matrix<float> C = A;
+
+        for (int i = 0; i < randomPoints; i++)
+        {
+            int randomColumn = Random.Range(0, C.ColumnCount);
+            int randomRow = Random.Range(0, C.RowCount);
+
+            C[randomRow, randomColumn] = Mathf.Clamp(C[randomRow, randomColumn] + Random.Range(-1f, 1f), -1f, 1f);
+        }
+
+        return C;
+
+    }
+
+    // Crossovers the Neural Networks weights and biases
+    private void Crossover(NeuralNetwork[] newPop)
+    {
+        for (int i = 0; i < numCrossover; i += 2)
+        {
+            int AIndex = i;
+            int BIndex = i + 1;
+
+            if (genePool.Count >= 1)
+            {
+                for (int l = 0; l < 100; l++)
+                {
+                    AIndex = genePool[Random.Range(0, genePool.Count)];
+                    BIndex = genePool[Random.Range(0, genePool.Count)];
+
+                    if (AIndex != BIndex)
+                        break;
+                }
+            }
+
+            NeuralNetwork Child1 = new NeuralNetwork();
+            NeuralNetwork Child2 = new NeuralNetwork();
+
+            Child1.Init(boatController.layers, boatController.neurons);
+            Child2.Init(boatController.layers, boatController.neurons);
+
+            Child1.fitness = 0;
+            Child2.fitness = 0;
+
+
+            for (int w = 0; w < Child1.weights.Count; w++)
+            {
+
+                if (Random.Range(0.0f, 1.0f) < 0.5f)
+                {
+                    Child1.weights[w] = population[AIndex].weights[w];
+                    Child2.weights[w] = population[BIndex].weights[w];
+                }
+                else
+                {
+                    Child2.weights[w] = population[AIndex].weights[w];
+                    Child1.weights[w] = population[BIndex].weights[w];
+                }
+
+            }
+
+
+            for (int w = 0; w < Child1.biases.Count; w++)
+            {
+
+                if (Random.Range(0.0f, 1.0f) < 0.5f)
+                {
+                    Child1.biases[w] = population[AIndex].biases[w];
+                    Child2.biases[w] = population[BIndex].biases[w];
+                }
+                else
+                {
+                    Child2.biases[w] = population[AIndex].biases[w];
+                    Child1.biases[w] = population[BIndex].biases[w];
+                }
+
+            }
+
+            newPop[naturallySelected] = Child1;
+            naturallySelected++;
+
+            newPop[naturallySelected] = Child2;
+            naturallySelected++;
+
+        }
+    }
+
+    // Select the best in the population
+    private NeuralNetwork[] BestPopulation()
+    {
+
+        NeuralNetwork[] newPop = new NeuralNetwork[initPopulation];
+
+        for (int i = 0; i < bestSelection; i++)
+        {
+            newPop[naturallySelected] = population[i].InitCopy(boatController.layers, boatController.neurons);
+            newPop[naturallySelected].fitness = 0;
+            naturallySelected++;
+
+            int f = Mathf.RoundToInt(population[i].fitness * 10);
+
+            for (int c = 0; c < f; c++)
+            {
+                genePool.Add(i);
+            }
+
+        }
+
+        for (int i = 0; i < worstSelection; i++)
+        {
+            int last = population.Length - 1;
+            last -= i;
+
+            int f = Mathf.RoundToInt(population[last].fitness * 10);
+
+            for (int c = 0; c < f; c++)
+            {
+                genePool.Add(last);
+            }
+
+        }
+
+        return newPop;
+
+    }
+
+    // Sort the Neural Netowrk population
+    private void SortPopulation()
+    {
+        for (int i = 0; i < population.Length; i++)
+        {
+            for (int j = i; j < population.Length; j++)
+            {
+                if (population[i].fitness < population[j].fitness)
+                {
+                    NeuralNetwork temp = population[i];
+                    population[i] = population[j];
+                    population[j] = temp;
+                }
+            }
+        }
+
+    }
+}
---
+++ Assets/Scripts/NeuralNetwork.cs
@@ -0,0 +1,172 @@
+﻿// Saran Krishnaraja 100621699
+// Justin Collier 100345263
+// Jarvis Ortega 100625970
+// Bosco Kan 100621465
+
+
+// Code referenced from:
+// https://www.youtube.com/watch?v=C6SZUU8XQQ0
+// https://www.youtube.com/watch?v=VYQZ-kjP1ec
+// https://www.youtube.com/watch?v=J3_iAC1q1NM&list=WL&index=94&t=0s
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+using MathNet.Numerics.LinearAlgebra;
+
+using Random = UnityEngine.Random;
+
+public class NeuralNetwork : MonoBehaviour
+{
+    // Variables needed for the Neural Network
+    public Matrix<float> inputLayer = Matrix<float>.Build.Dense(1, 3);
+
+    public List<Matrix<float>> hiddenLayers = new List<Matrix<float>>();
+
+    public Matrix<float> outputLayer = Matrix<float>.Build.Dense(1, 2);
+
+    public List<Matrix<float>> weights = new List<Matrix<float>>();
+
+    public List<float> biases = new List<float>();
+
+    public float fitness;
+
+    // Initialize the Neural Network
+    public void Init(int hiddenLayer, int hiddenNeuron)
+    {
+        inputLayer.Clear();
+        hiddenLayers.Clear();
+        outputLayer.Clear();
+        weights.Clear();
+        biases.Clear();
+
+        for (int i = 0; i < hiddenLayer + 1; i++)
+        {
+
+            Matrix<float> f = Matrix<float>.Build.Dense(1, hiddenNeuron);
+
+            hiddenLayers.Add(f);
+
+            biases.Add(Random.Range(-1f, 1f));
+
+            //WEIGHTS
+            if (i == 0)
+            {
+                Matrix<float> inputToH1 = Matrix<float>.Build.Dense(3, hiddenNeuron);
+                weights.Add(inputToH1);
+            }
+
+            Matrix<float> HiddenToHidden = Matrix<float>.Build.Dense(hiddenNeuron, hiddenNeuron);
+            weights.Add(HiddenToHidden);
+
+        }
+
+        Matrix<float> OutputWeight = Matrix<float>.Build.Dense(hiddenNeuron, 2);
+        weights.Add(OutputWeight);
+        biases.Add(Random.Range(-1f, 1f));
+
+        RandomWeights();
+    }
+
+    // Initialize a copy of the Neural Network
+    public NeuralNetwork InitCopy(int hiddenLayer, int hiddenNeuron)
+    {
+        NeuralNetwork n = new NeuralNetwork();
+
+        List<Matrix<float>> newWeights = new List<Matrix<float>>();
+
+        for (int i = 0; i < this.weights.Count; i++)
+        {
+            Matrix<float> currentWeight = Matrix<float>.Build.Dense(weights[i].RowCount, weights[i].ColumnCount);
+
+            for (int x = 0; x < currentWeight.RowCount; x++)
+            {
+                for (int y = 0; y < currentWeight.ColumnCount; y++)
+                {
+                    currentWeight[x, y] = weights[i][x, y];
+                }
+            }
+
+            newWeights.Add(currentWeight);
+        }
+
+        List<float> newBiases = new List<float>();
+
+        newBiases.AddRange(biases);
+
+        n.weights = newWeights;
+        n.biases = newBiases;
+
+        n.InitHidden(hiddenLayer, hiddenNeuron);
+
+        return n;
+    }
+
+    // Initialize the hidden layer and neuron
+    public void InitHidden(int hiddenLayer, int hiddenNeuron)
+    {
+        inputLayer.Clear();
+        hiddenLayers.Clear();
+        outputLayer.Clear();
+
+        for (int i = 0; i < hiddenLayer + 1; i++)
+        {
+            Matrix<float> newHiddenLayer = Matrix<float>.Build.Dense(1, hiddenNeuron);
+            hiddenLayers.Add(newHiddenLayer);
+        }
+
+    }
+
+    // Randomize the weights
+    public void RandomWeights()
+    {
+
+        for (int i = 0; i < weights.Count; i++)
+        {
+
+            for (int x = 0; x < weights[i].RowCount; x++)
+            {
+
+                for (int y = 0; y < weights[i].ColumnCount; y++)
+                {
+
+                    weights[i][x, y] = Random.Range(-1f, 1f);
+
+                }
+
+            }
+
+        }
+
+    }
+
+    // Run the network and output the acceleration and steering
+    public (float, float) PlayNetwork(float a, float b, float c)
+    {
+        inputLayer[0, 0] = a;
+        inputLayer[0, 1] = b;
+        inputLayer[0, 2] = c;
+
+        inputLayer = inputLayer.PointwiseTanh();
+
+        hiddenLayers[0] = ((inputLayer * weights[0]) + biases[0]).PointwiseTanh();
+
+        for (int i = 1; i < hiddenLayers.Count; i++)
+        {
+            hiddenLayers[i] = ((hiddenLayers[i - 1] * weights[i]) + biases[i]).PointwiseTanh();
+        }
+
+        outputLayer = ((hiddenLayers[hiddenLayers.Count - 1] * weights[weights.Count - 1]) + biases[biases.Count - 1]).PointwiseTanh();
+
+        //First output is acceleration and second output is steering
+        return (Sigmoid(outputLayer[0, 0]), (float)Math.Tanh(outputLayer[0, 1]));
+    }
+
+    // Sigmoid function used in PlayNetwork
+    private float Sigmoid(float s)
+    {
+        return (1 / (1 + Mathf.Exp(-s)));
+    }
+}
